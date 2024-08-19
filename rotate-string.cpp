4f5af64b498795cb1850267e8fc0999815c349fa
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2024-08-19 21:35:19 +0530

    Delete rotate-string.cpp

--- rotate-string.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) return false;
-        String str=goal+goal;
-        if(str.contains(s)) return true;
-        else return false;
-    }
-}
