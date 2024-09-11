810c04c56a61d7daae7c3721a84e0fd06b390e7a
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2024-09-11 18:59:32 +0530

    Add coin-change-ii.cpp

---
+++ coin-change-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int[][] dp;
+    public int change(int amount, int[] coins) {
+        dp = new int[coins.length+1][amount+1];
+        for(int[] rows : dp)
+            Arrays.fill(rows,-1);
+        return helper(coins, amount, coins.length);
+    }
+    public int helper(int [] coins, int amount,int n)
+    {
+        if(amount == 0)
+            return 1;
+        if(n == 0)
+            return 0;
+        if(dp[n][amount]!=-1)
+            return dp[n][amount];
+        if(coins[n-1]<=amount)
+            return dp[n][amount] = helper(coins,amount-coins[n-1],n) + helper(coins,amount,n-1);
+        else
+            return dp[n][amount] =  helper(coins,amount,n-1);
+    }
+}
