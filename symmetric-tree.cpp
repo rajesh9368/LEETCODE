80da35fcd0989d1fcce1023cd1052f25a5d7faf6
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2025-01-02 13:37:42 +0530

    Add symmetric-tree.cpp

---
+++ symmetric-tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean operation(TreeNode root1,TreeNode root2){
+        if(root1 == null && root2 ==null) return true;
+        if((root1!=null && root2==null) ||(root1==null && root2!=null)) return false;
+        if(root1.val!=root2.val) return false;
+        return operation(root1.left,root2.right) && operation(root1.right,root2.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return operation(root,root);
+    }
+}
