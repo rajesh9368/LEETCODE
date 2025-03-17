c2dd53e07731db105af1edb6befabadd472deb52
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2025-03-18 01:03:01 +0530

    Add subsets.cpp

---
+++ subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public void operation(int n,int[] nums,int i,ArrayList<Integer> temp,List<List<Integer>> ans){
+        if(i>=n){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[i]);
+        operation(n,nums,i+1,temp,ans);
+        temp.remove(temp.size()-1);
+        operation(n,nums,i+1,temp,ans);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        ArrayList<Integer> temp = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        operation(n,nums,0,temp,ans);
+        return ans;
+    }
+}
