225f42018a5f24cd1c34a982e0c4e167e38cf55a
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2025-05-09 11:26:37 +0530

    Add reverse-linked-list-ii.java

---
+++ reverse-linked-list-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode reverseLL(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode last = reverseLL(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right || head==null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for(int i=1;i<left;i++) prev = prev.next;
+        ListNode start=prev.next, end=start;
+        for(int i=left;i<right;i++) end=end.next;
+        ListNode res = end.next;
+        end.next = null;
+        ListNode reversed = reverseLL(start);
+        prev.next=reversed;
+        start.next = res;
+        return dummy.next;
+    }
+}
