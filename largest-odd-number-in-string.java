b95a03fc23b8580e5c7f218d24b21b50f5e19ea1
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2025-08-02 11:15:37 +0530

    Add largest-odd-number-in-string.java

---
+++ largest-odd-number-in-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int len = num.length();
+        for(int i=len-1;i>=0;i--){
+            int no = (int)num.charAt(i)-'0';
+            if(no%2!=0) return num.substring(0,i+1);
+        }
+        return "";
+    }
+}
